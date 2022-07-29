85bded87fb3296010b379e854d1e90202d2c38b4
MJ-CJM <18252010292@163.com>
2022-07-30 00:41:31 +0800

    7-30 commit

---
+++ 每日一题/6.25~752. 打开转盘锁.go
@@ -0,0 +1,10 @@
+// -*- coding:utf-8 -*-
+// @Time : 2021/6/26 2:53 下午
+// @Author: MJ-CJM
+// @File : leetcode/6.25~752. 打开转盘锁
+package main
+
+func openLock(deadends []string, target string) int {
+	return 0
+}
+
